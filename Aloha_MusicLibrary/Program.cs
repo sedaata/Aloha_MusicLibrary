b967114ad5cc74f0a7a002123a77786aaac14be2
cagatayyildiz <yildiz.cagatay@hotmail.com>
2023-08-02 22:36:08 +0300

    project init

---
+++ Aloha_MusicLibrary/Program.cs
@@ -0,0 +1,60 @@
+﻿using Aloha_MusicLibrary.Services;
+
+namespace Aloha_MusicLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = new ServiceCollection()
+            .AddDbContext<MusicStoreContext>()
+            .AddScoped<ArtistService>()
+            .AddScoped<SongService>()
+            .AddScoped<PlaylistService>()
+            .AddScoped<PlaylistSongService>()
+            .BuildServiceProvider();
+
+            var artistService = serviceProvider.GetRequiredService<ArtistService>();
+            var songService = serviceProvider.GetRequiredService<SongService>();
+            var playlistService = serviceProvider.GetRequiredService<PlaylistService>();
+            var playlistSongService = serviceProvider.GetRequiredService<PlaylistSongService>();
+
+            // Yukarıdaki kodlar proje için gereklidir.
+            // 1. Entity Framework ve DbContext sınıfıyla birlikte projenizi hazırlayınız.
+            // 2. Models klasörü içinde yukarıda servisleri yer alan entityleri oluşturunuz, gerekli ilişkilendirmelerini kurgulayarak uygulayınız, Code First yöntemi ile ekleyiniz ve Migration'larını uygulayınız;
+            ///// Artist [Name, Genre (müzik türü)]
+            ///// Song [Title, Duration (çalma süresi), ReleaseYear, ArtistId]
+            ///// Playlist [Name, Description]
+            // 3. Services klasörü içine yukarıda gerekli olan servisleri ekleyiniz. Her bir entity için ayrı servis kullanınız. Bağlantı için klasördeki örnek servisten faydalanabilirsiniz.
+            // 4. Aşağıdaki proje gereklilikleri kısmındaki adımları uygulamak için ilgili servislere gerekli metotları ekleyip bu sayfada kullanınız.
+            // Önemli Not!!!: Yukarıdaki serviceProvider yapıları CRUD işlemlerinizi bu sayfada uygulamak için herhangi bir context sınıfına ihtiyaç duymayacağınız şekilde yeterlidir. Örnek: artistService.AddArtist(newArtist);
+
+
+            // Proje Gereklilikleri
+            // Tek bir oturumda konsol uygulamasının gerçekleştirmesi gereken görevler;
+            // 1. Tüm Şarkıları Listele
+            // 2. Şarkı Ekle (Sanatçı olması gerekiyor)
+            // 3. Şarkıyı Düzenle
+            // 4. Şarkıyı Sil
+            // 5. Tüm Sanatçıları Listele
+            // 6. Sanatçı Ekle
+            // 7. Sanatçıyı Düzenle
+            // 8. Sanatçıyı Sil
+            // 9. Tüm Çalma Listelerini Listele
+            // 10. Çalma Listesini Görüntüle (Seçilen liste içindeki şarkıları göstermelidir)
+            // 11. Çalma Listesi Ekle
+            // 12. Çalma Listesini Düzenle
+            // 13. Çalma Listesini Sil
+            // 14. Çalma Listesine Şarkı Ekle
+            // 15. Çalma Listesinden Şarkı Kaldır
+            // 16. Çıkış
+
+            // Konsol uygulamanızı yukarıdaki tüm adımları tek tek sağlayacak şekilde burada yazınız.
+            ////// Uygulamanın her adımı aşağıda oluşturulacak metotları kullanmalıdır. Örnek: Tüm Şarkıları Listele: ListAllSongs(artistService, songService)
+        }
+
+        // Konsol uygulamasının kullanacağı metotları sırasıyla buraya yazınız.
+        ////// Örnek: private static void ListAllSongs(ArtistService artistService, SongService songService)
+        ///////// Not: Dışarıdan alınan servis parametreleri kullanacağınız CRUD işlemleri için gereklidir. Her bir adımın kullanıcı arayüzü için gerekli Write ve Read yapılarını bu metotların içinde kullanınız.
+    }
+}
---
+++ Aloha_MusicLibrary/Services/ArtistService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aloha_MusicLibrary.Services
+{
+    public class ArtistService
+    {
+        private readonly MusicStoreContext _context;
+
+        public ArtistService(MusicStoreContext context)
+        {
+            _context = context;
+        }
+
+        // CRUD ve gerekli diğer metotları buraya yazınız. Bu sayfada sadece Artist ile ilgili işlemler uygulanmalıdır. Diğer entityler için ayrı servisler oluşturulmalıdır.
+        // _context.Artists.Add()
+    }
+}
